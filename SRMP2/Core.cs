90199bf8ed0731fcb3bf6367d77d4727ca977c10
CacheNetworks <43885817+cachenetworks@users.noreply.github.com>
2026-09-13 22:39:14 +1000

    Add files via upload

---
+++ SRMP2/Core.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(SRMP2.Core), "SRMP2", "1.0.0", "lordp", null)]
+[assembly: MelonGame("MonomiPark", "SlimeRancher2")]
+
+namespace SRMP2
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
